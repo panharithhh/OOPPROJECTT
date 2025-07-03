e27681549da2ae57acb4fe83d8ca9283eec93fd6
panharithhh <161872594+panharithhh@users.noreply.github.com>
2025-07-03 13:54:30 +0700

    test

---
+++ test.js
@@ -0,0 +1,114 @@
+// console.log("script js")
+// const propmt = require("prompt-sync")()
+
+// const v = prompt("type something")
+
+// console.log(v)
+
+// string; 
+// Boolean  
+// number
+// undefined
+// null
+// BigInt
+
+// var x = "helloWorld"; // with var is that it is a global scope 
+// hello = "lol"
+
+// let y = "r";
+// y = 7 
+// // can change later on inside the block not a global thing just like const 
+
+
+// function test(){
+//     console.log(x)
+
+//     var x = "test"
+//     console.log(x)
+// }
+
+// test()
+
+
+// const x = 6 // convert the x to a string 
+
+// const y = "7"
+
+// console.log(x + y)
+
+
+
+
+// so in js will convert into a string with + and - and * / will convert into an int 
+
+// == loose 
+
+// console.log(true == 1)
+// console.log("" = i)
+// const cond = 2 < 3 ? " lol" : " not lol"
+// console.log(cond)
+
+// const value = 1;
+
+// switch(value){
+//     case 1:
+//         console.log("not so cool")
+//         break;  
+//     case 2 : 
+//         console.log('kdorhgng')
+//          break;  
+//     default:
+//         console.log("enter the right number ")
+
+// }
+
+const  lol = []
+
+daf  = []
+
+const arr = [1,2,3,4,true]
+const arr2 = Array.from("hello")
+
+arr2[0] = "y"
+
+console.log(arr[0])
+
+arr.push(12)
+
+arr.pop(1)
+
+arr2.unshi
+
+
+// const obj = {
+//     name: "alice",
+//     age : 23,
+//     sayHello : function(){
+//         return 2;
+//     }
+
+// }
+
+// obj.age = 'tim'
+// ob
+
+// class Student { 
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() { 
+//         console.log(`hello my name is ${this.name} and my age is ${this.age}`)
+//     }
+// }
+
+
+// const student1 = new Student("Alice", 20);
+// student1.greet(); // Output: hello my name is Alice and my age is 20
+
+// const map = new Map();
+
+// const number = [1,2,3,4,5];
+
+
